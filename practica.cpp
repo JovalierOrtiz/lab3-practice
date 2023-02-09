ffc977bbd8461fc883b2edc072a8ab2899ce9bbd
JovalierOrtiz <Jovalier.ortiz@upr.edu>
2023-02-08 23:26:35 -0400

    Class person init

--- practica.cpp
+++ practica.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class person
+{
+
+    public:
+    int age;
+    int weigth;
+
+
+};
+
 int main()
 {
     int num = 12;
+
+    person a;
+    a.age = 12;
+    a.weigth = 112;
+
+    cout<<endl<<a.age<<endl<<a.weigth;
     
-    cout<<'\n'<<num;
     
 
     return 0;
